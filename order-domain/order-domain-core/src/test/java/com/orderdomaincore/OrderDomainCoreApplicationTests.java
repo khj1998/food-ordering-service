a0465e3486299689e04e33fdc8112e0cc6e8f99b
kimhojin98@naver.com <kimhojin98@naver.com>
2024-08-31 23:38:27 +0900

    Feat : Create multi module project for food ordering service

---
+++ order-domain/order-domain-core/src/test/java/com/orderdomaincore/OrderDomainCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderdomaincore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDomainCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
